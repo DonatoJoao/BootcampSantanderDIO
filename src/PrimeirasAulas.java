5c3cd8cd334ac660b71fb3865028b0bef64892fb
DonatoJoao <joaod.morais@gmail.com>
2024-04-23 20:43:52 -0300

    Initial commit

---
+++ src/PrimeirasAulas.java
@@ -0,0 +1,11 @@
+public class PrimeirasAulas {
+
+    public static void main(String[] args) {
+        System.out.println("oi");
+        System.out.println(welcome("Olá, Deus!"));
+    }
+    public static String welcome(String saudacao){
+        return saudacao;
+    }
+    
+}
