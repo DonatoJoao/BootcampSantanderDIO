7e96f32b774e71c8e99f6679ca4b521a62ffcf58
DonatoJoao <joaod.morais@gmail.com>
2024-04-23 22:14:42 -0300

    Praticando exercícios da aula 2 e bricando um pouco com os métodos da classe String... esta ultima não fazia parte da aula, porém aproveitei pra praticar um pouco mais.

--- src/edu/joao/aula2_TiposEVariaveis/Main.java
+++ src/edu/joao/aula2_TiposEVariaveis/Main.java
@@ -1,5 +1,31 @@
 package edu.joao.aula2_TiposEVariaveis;
 
 public class Main {
-    //aguardando conteúdo :)
+    public static void main(String[] args) {
+        //variaveis
+        byte numero = 123;
+        int idade; //variavel do tipo inteiro sem valor atribuído
+        int anoDeFabricacao = 2020; // variavel com valor inicializado
+        double salarioMinimo = 7500.00; //variavel do tipo double com valor de um sonho :)
+        long valorMeuSalario = 9000999876543L; //tipo long deve terminar com L
+        String nome = "Donato";
+        String cpfDonato = "789456123-00";
+
+        //constantes em Java
+        final double PI = 3.14; // valor não alterável e por convenção, usar UPPERCASE para nomear constantes
+        final String cpfJoao = "123456789-99";
+
+        //Improvisando e testando "métodos" (?) da classe String
+        System.out.println("Quantidade de caracteres desse cpf: " + cpfJoao.length());
+        System.out.println(cpfJoao.compareTo("123456789-99")); //
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.toUpperCase());
+        System.out.println(nome.concat(cpfDonato));
+        System.out.println(nome.contains("D"));
+        System.out.println(nome.contains("d"));
+        System.out.println(nome.toLowerCase().contains("d"));
+
+
+
+    }
 }
