83f5dcc8ce9cc85d92a39dc310ed9754aea1074c
DonatoJoao <joaod.morais@gmail.com>
2024-05-11 18:33:01 -0300

    Finalizando o Desafio:  Contador

--- src/edu/joao/Desafios/ControleDeFluxo/Contador.java
+++ src/edu/joao/Desafios/ControleDeFluxo/Contador.java
@@ -1,41 +1,43 @@
 package edu.joao.Desafios.ControleDeFluxo;
 
+import javax.sound.sampled.SourceDataLine;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Contador {
 
-
-    //TERMINAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
     public static void main(String[] args) throws ParametrosInvalidosException {
 
         Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
+        int parametroUm = 0;
+        int parametroDois = 0;
+        try {
+            System.out.println("Digite o primeiro número");
+            parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo número maior que o primeiro");
+            parametroDois = terminal.nextInt();
+        } catch (InputMismatchException e){
+            System.out.println("Você deve digitar somente números");
+        }
 
-        //TODO: Implementar dentro do try/catch e capturar/tratar exceção
-        contar(parametroUm, parametroDois);
 
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception){
+               throw new ParametrosInvalidosException(exception);
+        }
     }
+    private static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
 
-    //Aqui ta ok! Lógica funcionando, agora falta TODO acima
-    private static void contar(int parametroUm, int parametroDois) {
         if (parametroUm<parametroDois){
             int contagem = parametroDois-parametroUm;
             for (int i = 0; i < contagem; i++) {
                 System.out.println("iteração " + (i+1) + " é o numero: " + (i+parametroUm));
             }
-
         } else {
-            System.out.println("O segundo numero deve ser maior que o primeiro");
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
-
     }
-
-
 }
 
 
--- src/edu/joao/Desafios/ControleDeFluxo/ParametrosInvalidosException.java
+++ src/edu/joao/Desafios/ControleDeFluxo/ParametrosInvalidosException.java
@@ -2,12 +2,7 @@ package edu.joao.Desafios.ControleDeFluxo;
 
 public class ParametrosInvalidosException extends Exception {
 
-      //COMO CRIAR E USAR ISSO NO DESAFIO?
-        static String validaEntrada(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-            if (parametroUm>parametroDois){
-                throw new ParametrosInvalidosException();
-            }
-            return "O primeiro parametro deve ser menor que o segundo";
-        }
+    public ParametrosInvalidosException(ParametrosInvalidosException exception) {
+    }
 
 }
